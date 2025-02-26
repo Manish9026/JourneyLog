54cb8fdd968a525cb809d6cf8fb6a91b84221b12
Manish9026 <135260553+Manish9026@users.noreply.github.com>
2025-02-26 18:34:41 +0530

    update backend feature for deleteRecord propup

--- frontend/src/pages/statement/Statement.jsx
+++ frontend/src/pages/statement/Statement.jsx
@@ -579,7 +579,7 @@ const CustomRecordSelect=({endDate,startDate,title,isSlected=false,onClick})=>{
 const PingDot = ({ size = 10 }) => {
 
   return (
-    <span className={`relative flex size-[${size+"px"}] `}>  <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-sky-600 opacity-75"></span>  <span  className={`relative inline-flex size-[${size+"px"}] rounded-full bg-sky-500`}></span></span>
+    <span style={{width:`${size}px`,height:`${size}px`}} className={`relative flex size-[${size+"px"}] `}>  <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-sky-600 opacity-75"></span>  <span style={{width:`${size}px`,height:`${size}px`}}  className={`relative inline-flex size-[${size+"px"}] rounded-full bg-sky-500`}></span></span>
   )
 }
 
